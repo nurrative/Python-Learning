ba2ffc50d4aeb64487dd5c76acf4da0ffcf2b3a7
nurrative <nursul1997@gmail.com>
2023-04-05 11:53:40 +0600

    sqlalchemy

--- http_orm/sqlalchemy_test/main.py
+++ http_orm/sqlalchemy_test/main.py
@@ -17,6 +17,9 @@ class Product(Base):
     title = Column(String)
     price = Column(Integer)
 
+    def __repr__(self) -> str:
+        return f"{self.id} -> {self.title} <-> {self.price} "
+
 Base.metadata.create_all(bind=engine)
 # записываем таблицу в бд
 
@@ -28,11 +31,45 @@ session = SessionLoocal()
 
 new_product = Product(title = 'product1',price = 120)
 # создаем продукт(запись в таблицу)
-#  для орм создаем запрос на запись в таблицу
+# для орм создаем запрос на запись в таблицу
 
-session.add(new_product)
+# session.add(new_product)
 # добавляем запрос в сессию
 
-session.commit()
+# session.commit()
 # отправляем набор запросов бд
+
+# products = session.query(Product).all()
+# # получаем все записи из таблиы product
+# print(products)
+
+# session.add(Product(title='product2', price=34))
+# session.add(Product(title='product3', price=245))
+# session.add(Product(title='product4', price=45))
+# session.add(Product(title='product5', price=345))
+# session.commit()
+products = session.query(Product).all()
+# получаем все записи из таблиы product
+print(products)
+
+products = session.query(Product).filter(Product.price>100).all()
+# получаем все записи из таблиы product у которых цена больше 100
+print(products)
+
+product3 = session.query(Product).get(3)
+# получаем продукт под id 3
+print(product3)
+
+# product4 = session.query(Product).get(4)
+# session.delete(product4)
+# session.commit()
+
+product3.title = 'new title'
+product3.price = 100
+
+session.commit()
+
+products = session.query(Product).all()
+# получаем все записи из таблиы product
+print(products)
 
